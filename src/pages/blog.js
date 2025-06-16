607624a696027a4a03b22aa90106a18e5fd6ad4b
Nieve2205 <166433352+Nieve2205@users.noreply.github.com>
2025-06-16 10:07:06 -0500

    sitemap

---
+++ src/pages/blog.js
@@ -0,0 +1,8 @@
+export default function Blog() {
+  return (
+    <main style={{ padding: "2rem" }}>
+      <h1>Blog</h1>
+      <p>Bienvenido al blog. Próximamente artículos y noticias.</p>
+    </main>
+  );
+}
---
+++ src/pages/contacto.js
@@ -0,0 +1,8 @@
+export default function Contacto() {
+  return (
+    <main style={{ padding: "2rem" }}>
+      <h1>Contacto</h1>
+      <p>Puedes contactarnos a través del correo contacto@mi-sitio.com.</p>
+    </main>
+  );
+}
