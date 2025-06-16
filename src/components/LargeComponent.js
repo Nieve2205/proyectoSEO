bacabaecf47806345002ce85bb2c63081410982b
Nieve2205 <166433352+Nieve2205@users.noreply.github.com>
2025-06-16 09:57:44 -0500

    se implementa  lazy loading y optimizacion de fuentes

---
+++ src/components/LargeComponent.js
@@ -0,0 +1,7 @@
+export default function LargeComponent() {
+  return (
+    <div style={{ height: "400px", background: "#eee", padding: "1rem" }}>
+      <h2>Componente cargado dinámicamente</h2>
+    </div>
+  );
+}
