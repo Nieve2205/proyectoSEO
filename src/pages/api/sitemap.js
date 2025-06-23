51b7401d888f3afdccbbec711a352753cdfe9130
Nieve2205 <166433352+Nieve2205@users.noreply.github.com>
2025-06-22 21:39:55 -0500

    ajustes para sitemap

--- src/pages/api/sitemap.js
+++ src/pages/api/sitemap.js
@@ -1,32 +1,16 @@
-import { globby } from "globby";
-import path from "path";
-
 const BASE_URL = "https://proyecto-seo.vercel.app";
 
 export default async function handler(req, res) {
-  const pages = await globby([
-    "pages/**/*.js",
-    "!pages/_*.js",
-    "!pages/api/**",
-  ]);
-
-  const urls = pages.map((file) => {
-    const route = file
-      .replace("pages", "")
-      .replace(".js", "")
-      .replace("/index", "")
-      .replace(/\\/g, "/");
-
-    return `${BASE_URL}${route === "" ? "/" : route}`;
-  });
+  const urls = ["/", "/blog", "/contacto"]; // Agrega más rutas dinámicamente
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     ${urls
-      .map((url) => `<url><loc>${url}</loc></url>`)
+      .map((url) => `<url><loc>${BASE_URL}${url}</loc></url>`)
       .join("")}
   </urlset>`;
 
   res.setHeader("Content-Type", "application/xml");
-  res.status(200).send(sitemap);
+  res.write(sitemap);
+  res.end();
 }
