ce09a8b4cb28a99a8b34ee3f5ca5306423750375
Nieve2205 <166433352+Nieve2205@users.noreply.github.com>
2025-06-22 22:10:06 -0500

    tarea

--- src/pages/api/sitemap.js
+++ src/pages/api/sitemap.js
@@ -2,9 +2,12 @@ const BASE_URL = 'https://proyecto-seo.vercel.app';
 
 export default async function handler(req, res) {
   const staticPages = ['', '/blog', '/contacto'];
+
+  // Datos simulados
   const posts = [
     { slug: 'post-1' },
-    { slug: 'post-2' }
+    { slug: 'post-2' },
+    { slug: 'post-3' }
   ];
 
   const urls = [
@@ -13,9 +16,9 @@ export default async function handler(req, res) {
   ].join('');
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    ${urls}
-  </urlset>`;
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  ${urls}
+</urlset>`;
 
   res.setHeader('Content-Type', 'application/xml');
   res.status(200).end(sitemap);
